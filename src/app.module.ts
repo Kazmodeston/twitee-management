c948644a75d44f8767d4fc4ab70e027a0e0928a4
Muhammad Kazim <kazmodeston1@gmail.com>
2023-01-13 21:50:13 +0100

    created user controller, service and imported to users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [],
       synchronize: true,
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
